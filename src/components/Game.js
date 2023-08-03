94efa44a9d3aefd447737648bf706d6df76746db
harshal <kardileharshal567@gmail.com>
2023-08-03 12:30:46 +0530

    completed bugfixes by adding condition of match tied and restart button to start game again

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { calculateWinner } from '../helpers';
 import Board from './Board';
 
-const styles={
+const styles = {
 
     width:'200px',
     margin:'20px auto'
@@ -14,9 +14,10 @@ const Game = () => {
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [stepNumber, setStepNumber] = useState(0);
     const [xIsNext, setXisNext] = useState(true);
+    const [gameInProgress, setGameInProgress] = useState(true);
     const winner = calculateWinner(history[stepNumber]);
 
-    
+     
     const handleClick = i => {
         const timeInHistory = history.slice(0, stepNumber + 1);
         const current = timeInHistory[stepNumber];
@@ -30,20 +31,31 @@ const Game = () => {
         setXisNext(!xIsNext);   
     }
 
-    const jumpTo = move =>{
+    const checkForTie = squares => {
+        return squares.every(square => square !== null);
+      };
+      
+
+    const jumpTo = move => {
         setStepNumber(move);
-        setXisNext(move % 2 === 0)
+        setXisNext(move === 0)
     }
+
+    const restartGame = () => {
+        setHistory([Array(9).fill(null)]);
+        setStepNumber(0);
+        setXisNext(true);
+        setGameInProgress(true);
+      };
 
     const renderMoves = () => (
         history.map((_step, move) => {
-            const destination = move ? `Go to move #${ move }`: 'Go to start';
+            const destination = move ? `Go to move #${ move }`: 'Starting';
             return (
             <li key={ move }>
                 <button onClick = {() => jumpTo( move )}>{destination}</button>
             </li>
-            
-            )
+            ) 
         })
     )
 
@@ -51,7 +63,22 @@ const Game = () => {
         <>
         < Board squares={history[stepNumber]} onClick = {handleClick} /> 
         <div style={ styles }>
-            <p>{winner ? 'Winner: ' + winner : 'Next Player: '+ (xIsNext ? 'X' : 'O')}</p>
+            <p>
+                {winner ? (
+                    <p>Winner: {winner}</p>
+                ) : checkForTie(history[stepNumber]) ? (
+                    <p>It's a tie!</p>
+                ) : (
+                    <p>Next Player: {xIsNext ? 'X' : 'O'}</p>
+                )}
+            </p>
+            <p>
+                {gameInProgress ? (
+                <button onClick={restartGame}>Restart</button>
+                ) : (
+                <button onClick={() => setGameInProgress(true)}>Start New Game</button>
+                )}
+            </p>
             {renderMoves()}
         </div>
         </>   
